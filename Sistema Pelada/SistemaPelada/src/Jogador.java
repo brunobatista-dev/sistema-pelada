c257e4d74df2ca906c928f73e787693cec1071c3
guiaraujofreitas1504 <guiaraujofreitas1504@gmail.com>
2026-05-30 16:54:49 -0300

    Add files via upload
    Arquivo atualizado completo, falta adicionar o Whats/Email

---
+++ Sistema Pelada/SistemaPelada/src/Jogador.java
@@ -0,0 +1,13 @@
+public class Jogador {
+    private String nome = ""; 
+
+    // Getter para ler o nome
+    public String getNome() {
+        return this.nome;
+    }
+
+    // Setter para alterar/salvar o nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
